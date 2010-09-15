e699fe11ff05a0fb8671c7442b850c8624183b7e
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2010-09-15 19:31:23 +0000

    Added vignette: genomic plots for a t/n pair.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@2048 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/testScripts/system/pairedPSCN/R/00.setup.R
@@ -0,0 +1,31 @@
+library("aroma.affymetrix")
+library("aroma.cn");
+
+log <- verbose <- Arguments$getVerbose(-8, timestamp=TRUE)
+figForce <- TRUE;
+
+dsName <- "GSE20584";
+chipType <- "GenomeWideSNP_6";
+
+pairs <- matrix(c("GSM517071", "GSM517072"), ncol=2, byrow=TRUE);
+colnames(pairs) <- c("tumor", "normal");
+
+## paths
+rootPath <- "totalAndFracBData";
+rootPath <- Arguments$getWritablePath(rootPath);
+
+dsTags <- "ACC,ra,-XY,BPN,-XY,AVG,FLN,-XY";
+
+totalPath <- rootPath;
+fracBPath <- rootPath;
+
+dataSet <- sprintf("%s,%s", dsName, dsTags);
+
+figPath <- "figures";
+figPath <- file.path(figPath, postProcPath);
+figPath <- Arguments$getWritablePath(figPath);
+
+############################################################################
+## 2010-09-15
+## o Created.
+############################################################################
