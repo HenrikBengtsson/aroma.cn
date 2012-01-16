d80f8f4da166f73bc18f53752b242c9d442a2069
Henrik Bengtsson <hb@aroma-project.org>
2012-01-16 20:03:12 +0000

    Version: 1.0.1 [2012-01-16] o Added TotalCnBinnedSmoothing().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@2334 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/TotalCnBinnedSmoothing.R
@@ -0,0 +1,105 @@
+###########################################################################/**
+# @RdocClass TotalCnBinnedSmoothing
+#
+# @title "The TotalCnBinnedSmoothing class"
+#
+# \description{
+#  @classhierarchy
+#
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#  \item{...}{Arguments passed to @see "TotalCnSmoothing".}
+#  \item{robust}{If @TRUE, a robust smoother is used, otherwise not.}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+#
+# @author
+#*/########################################################################### 
+setConstructorS3("TotalCnBinnedSmoothing", function( ..., robust=FALSE) {
+  # Arguments 'robust':
+  robust <- Arguments$getLogical(robust);
+
+  extend(TotalCnSmoothing(...), "TotalCnBinnedSmoothing",
+    .robust = robust
+  );
+})
+
+
+setMethodS3("getParameters", "TotalCnBinnedSmoothing", function(this, ...) {
+  params <- NextMethod("getParameters", this, ...);
+  params$robust <- this$.robust;
+  params;
+}, private=TRUE);
+
+
+setMethodS3("getAsteriskTags", "TotalCnBinnedSmoothing", function(this, collapse=NULL, ...) {
+  tags <- NextMethod("getAsteriskTags", this, collapse=NULL, ...);
+
+  # Add class-specific tags
+
+  params <- getParameters(this);
+
+  # Parameter 'robust'
+  if (params$robust)
+    tags <- c(tags, "robust");
+
+  # Collapsed or split?
+  if (!is.null(collapse)) {
+    tags <- paste(tags, collapse=collapse);
+  } 
+
+  tags;
+}, protected=TRUE) 
+
+
+setMethodS3("smoothRawCopyNumbers", "TotalCnBinnedSmoothing", function(this, rawCNs, target, ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "Smoothing one set of copy numbers");
+  verbose && print(verbose, rawCNs);
+
+  # Setting up arguments
+  params <- getParameters(this);
+  params$FUN <- ifelse(params$robust, "median", "mean");
+  params$robust <- NULL;
+  args <- c(list(xOut=target$xOut), params, ...);
+
+  # Keep only known arguments
+  knownArguments <- names(formals(colBinnedSmoothing.matrix));
+  keep <- is.element(names(args), knownArguments);
+  args <- args[keep];
+  
+  args <- c(list(rawCNs), args);
+
+  verbose && cat(verbose, "Calling binnedSmoothing() with arguments:");
+  verbose && str(verbose, args);
+  args$verbose <- less(verbose, 20);
+  smoothCNs <- do.call("binnedSmoothing", args=args);
+
+  verbose && exit(verbose);
+
+  smoothCNs;
+}, protected=TRUE)
+
+
+
+############################################################################
+# HISTORY:
+# 2012-01-16
+# o Created from TotalCnKernelSmoothing.R.
+############################################################################
