932b71c26975677caff94a52811b8bf015adc267
Henrik Bengtsson <hb@aroma-project.org>
2012-02-27 02:23:42 +0000

    Version: 1.0.3 [2012-02-24] o CLEANUP: Moved some internal functions to the PSCBS package,   so package dependencies was also updated.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@2364 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/PairedPSCBS.CALL2.R
+++ R/PairedPSCBS.CALL2.R
@@ -11,7 +11,7 @@
 # @synopsis
 #
 # \arguments{
-#   \item{fit}{A PairedPSCBS fit object as returned by 
+#   \item{fit}{A @see "PSCBS::PairedPSCBS" fit object as returned by 
 #     @see "PSCBS::segmentByPairedPSCBS".}
 #   \item{maxScore}{A positive @double threshold. 
 #     If \code{"auto"}, the threshold is estimated empirically.}
@@ -21,9 +21,9 @@
 # }
 #
 # \value{
-#   Returns a PairedPSCBS fit object where columns for
-#   allelic imbalance scores and p-values as well as allelic
-#   balance calls are added.
+#   Returns a @see "PSCBS::PairedPSCBS" fit object
+#   where columns for allelic imbalance scores and
+#   p-values as well as allelic balance calls are added.
 # }
 #
 # @examples "../incl/callAllelicBalanceByBAFs.PairedPSCBS.Rex"
@@ -209,16 +209,16 @@ setMethodS3("callAllelicBalanceByBAFs", "PairedPSCBS", function(fit, maxScore="a
 # @synopsis
 #
 # \arguments{
-#   \item{fit}{A PairedPSCBS fit object as returned by 
+#   \item{fit}{A @see "PSCBS::PairedPSCBS" fit object as returned by 
 #     @see "PSCBS::segmentByPairedPSCBS".}
 #   \item{...}{Additional arguments passed to 
-#     @see "aroma.cn::findNeutralCopyNumberState".}
-#   \item{force}{If @TRUE, an already called object is skipped, otherwise not.}
-#   \item{verbose}{See @see "R.utils::Verbose".}
+#     @see "aroma.cn::findNeutralCopyNumberState" and
+#     @see "PSCBS::callCopyNeutral.PairedPSCBS".}
 # }
 #
 # \value{
-#   Returns a PairedPSCBS fit object where a column with the copy-neutral call.
+#   Returns a @see "PSCBS::PairedPSCBS" fit object
+#   where a column with the copy-neutral call.
 # }
 #
 # @examples "../incl/callCopyNeutralRegions.PairedPSCBS.Rex"
@@ -227,171 +227,26 @@ setMethodS3("callAllelicBalanceByBAFs", "PairedPSCBS", function(fit, maxScore="a
 #
 # @keyword internal
 #*/########################################################################### 
-setMethodS3("callCopyNeutralRegions", "PairedPSCBS", function(fit, ..., force=FALSE, verbose=FALSE) {
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
+setMethodS3("callCopyNeutralRegions", "PairedPSCBS", function(fit, ...) {
   # Call allelic balance or not, unless already done
-  fit <- callAllelicBalanceByBAFs(fit, ..., verbose=verbose);
-
-  segs <- getSegments(fit, splitters=TRUE);
-
-  # Nothing to do?
-  if (!force && !is.null(segs$neutralCall)) {
-    # Copy neutral segments are already called
-    return(fit);
-  }
-
-  C <- segs[,"tcnMean", drop=TRUE];
-  isAB <- segs[,"abCall", drop=TRUE];
-  n <- segs[,"tcnNbrOfSNPs", drop=TRUE]; # "tcnNbrOfLoci"? /HB 2010-09-09
-
-  # Give more weight to longer regions
-  weights <- n;
-
-  isNeutral <- findNeutralCopyNumberState(C=C, isAI=!isAB, weights=weights,
-                                                       ..., verbose=verbose);
-
-  segs$neutralCall <- isNeutral;
-
-  fitC <- fit;
-  fitC$output <- segs;
-  
-  fitC;
+  fit <- callAllelicBalanceByBAFs(fit, ...);
+  callCopyNeutral(fit, flavor="TCN|AB", ...);
 })
-
-
-
-setMethodS3("extractDhSegment", "PairedPSCBS", function(fit, idx, what=c("hets", "SNPs", "all"), ..., verbose=FALSE) {
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  # Argument 'what':
-  what <- match.arg(what);
-
-
-  segs <- getSegments(fit, splitters=TRUE);
-  stopifnot(!is.null(segs)); 
-  nbrOfSegments <- nrow(segs);
-
-  # Argument 'idx':
-  idx <- Arguments$getIndex(idx, max=nbrOfSegments);
-
-  # Argument 'verbose':
-  verbose <- Arguments$getVerbose(verbose);
-  if (verbose) {
-    pushState(verbose);
-    on.exit(popState(verbose));
-  } 
-
-
-
-  verbose && enter(verbose, "Extracting a specific DH segment");
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  # Extract the data and segmentation results
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  data <- getLocusData(fit);
-  stopifnot(!is.null(data));
-
-  segs <- getSegments(fit, splitters=TRUE);
-  stopifnot(!is.null(segs));
-
-  verbose && enter(verbose, "Subsetting segment");
-  # Subset the region-level data
-  seg <- segs[idx,,drop=FALSE];
-
-  isDivider <- all(is.na(seg));
-  if (isDivider) {
-    verbose && cat("Cannot extract DH segment. Not a valid segment: ", idx);
-    verbose && exit(verbose);
-    return(NULL);
-  }
-
-  verbose && print(verbose, seg);
-  verbose && cat(verbose, "Number of TCN markers: ", sum(seg[["tcnNbrOfLoci"]], na.rm=TRUE));
-  verbose && exit(verbose);
-
-  verbose && enter(verbose, "Subsetting data");
-  units <- seq(length=nrow(data));
-
-  # Keep only chromosome of interest
-  chr <- as.numeric(seg[,"chromosome"]);
-  if (!is.na(chr)) {
-    keep <- whichVector(data$chromosome == chr);
-    units <- units[keep];
-    data <- data[keep,];
-  }
-
-  # Keep only loci within the segment
-  xRange <- as.numeric(seg[,c("dhStart", "dhEnd")]);
-  keep <- whichVector(xRange[1] <= data$x & data$x <= xRange[2]);
-  units <- units[keep];
-  data <- data[keep,];
-
-  muN <- data$muN;
-  isSnp <- is.finite(muN);
-
-  # Keep only SNPs?
-  if (is.element(what, c("SNPs", "hets"))) {
-    keep <- whichVector(isSnp);
-    units <- units[keep];
-    data <- data[keep,];
-  }
-
-  # Keep only heterozygous SNPs?
-  if (what == "hets") {
-    isHet <- (muN == 1/2);
-    keep <- whichVector(isHet);
-    units <- units[keep];
-    data <- data[keep,];
-  }
-  verbose && exit(verbose);
-
-  n <- nrow(data);
-  verbose && cat(verbose, "Number of loci in DH segment: ", n);
-
-  # Special case?
-  listOfDhLociNotPartOfSegment <- fit$listOfDhLociNotPartOfSegment;
-  if (!is.null(listOfDhLociNotPartOfSegment)) {
-    tcnId <- seg[,"tcnId"];
-    dhId <- seg[,"dhId"];
-    dhLociNotPartOfSegment <- listOfDhLociNotPartOfSegment[[tcnId]];
-    if (!is.null(dhLociNotPartOfSegment)) {
-      lociToExclude <- dhLociNotPartOfSegment[[dhId]];
-      verbose && cat(verbose, "Excluding loci that belongs to a flanking segment: ", length(lociToExclude));
-      drop <- match(lociToExclude, units);
-      units <- units[-drop];
-      data <- data[-drop,];
-      n <- nrow(data);
-    }
-  }
-
-  verbose && cat(verbose, "Number of units: ", n);
-  verbose && cat(verbose, "Number of TCN markers: ", seg[,"tcnNbrOfLoci"]);
-
-  # Sanity check
-  if (what == "hets" && n > 0) stopifnot(n == seg[,"dhNbrOfLoci"]);
-
-  fitS <- fit;
-  fitS$data <- data;
-  fitS$output <- seg;
-
-  verbose && exit(verbose);
-
-  fitS;
-}, protected=TRUE) # extractDhSegment()
 
 
 
 ##############################################################################
 # HISTORY
+# 2012-02-24
+# o CLEANUP: Now callCopyNeutralRegions() calls callCopyNeutral(), which
+#   is now available in the PSCBS package.  The callCopyNeutralRegions()
+#   method will eventually be deprecated for the favor of the PSCBS one.
+# o Moved extractDhSegment() for PairedPSCBS to the PSCBS package.
 # 2012-02-23
 # o Moved drawC1C2Density() and plotC1C2Grid() to PairedPSCBS.PLOT3.R.
 # o Made extractDhSegment() protected.
 # 2011-12-15
-# o Turned of default memoization for callAllelicBalanceByBAFs().
+# o Turned off default memoization for callAllelicBalanceByBAFs().
 # o Now callAllelicBalanceByBAFs() for PairedPSCBS appends its parameter
 #   settings to the ones in the PairedPSCBS object.  Before it dropped
 #   them.
