5cfd81ba547c4457537d97963748c994d37c5f03
Henrik Bengtsson <hb@aroma-project.org>
2013-08-13 00:12:41 +0000

    Version: 1.4.1 [2013-08-12] o BUG FIX: byPath(), byName() and findByPath() for PairedPSCBSFileSet   was also affected by the bug described in the R-devel thread   'Do *not* pass '...' to NextMethod() - it'll do it for you; missing   documentation, a bug or just me?' on Oct 16, 2012. o BUG FIX: getPath() for PairedPscbsModel would throw an error on   getInputDataSet() not defined.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@3143 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/PairedPSCBSFileSet.R
+++ R/PairedPSCBSFileSet.R
@@ -2,52 +2,71 @@ setConstructorS3("PairedPSCBSFileSet", function(...) {
   extend(GenericDataFileSet(...), "PairedPSCBSFileSet");
 })
 
-setMethodS3("byPath", "PairedPSCBSFileSet", function(static, ..., pattern=".*(,PairedPSCBS[.]xdr|[.]RData)$") {
-  # Drop argument 'chipType'
-  args <- list(static=static, ..., pattern=pattern);
-  excl <- which("chipType" == names(args));
-  if (length(excl) > 0) {
-    args <- args[-excl];
-  }
+# setMethodS3("byPath", "PairedPSCBSFileSet", function(static, ..., pattern=".*(,PairedPSCBS[.]xdr|[.]RData)$") {
+#   # Drop argument 'chipType'
+#   args <- list(static=static, ..., pattern=pattern);
+#   excl <- which("chipType" == names(args));
+#   if (length(excl) > 0) {
+#     args <- args[-excl];
+#   }
+#
+#   # Call the "next" method
+#   args <- c(list("byPath"), args);
+#   do.call("NextMethod", args);
+# }, static=TRUE)
 
-  # Call the "next" method
-  args <- c(list("byPath"), args);
-  do.call("NextMethod", args);
+
+#setMethodS3("findByName", "PairedPSCBSFileSet", function(static, ..., chipType=NULL, paths="pscbsData/") {
+#  # Argument 'chipType':
+#  if (!is.null(chipType)) {
+#    chipType <- Arguments$getCharacter(chipType);
+#  }
+#
+#  # Argument 'paths':
+#  if (is.null(paths)) {
+#    paths <- eval(formals(findByName.PairedPSCBSFileSet)[["paths"]]);
+#  }
+#
+#  # Drop argument 'subdirs'
+#  args <- list(...);
+#  excl <- which("subdirs" == names(args));
+#  if (length(excl) > 0) {
+#    args <- args[-excl];
+#  }
+#  args <- c(list(static=static), args, subdirs=chipType, paths=paths);
+#
+#  # Call the "next" method
+#  args <- c(list("findByName"), args);
+#  do.call("NextMethod", args);
+#}, static=TRUE, protected=TRUE)
+
+setMethodS3("byPath", "PairedPSCBSFileSet", function(static, ..., pattern=".*(,PairedPSCBS[.]xdr|[.]RData)$") {
+  NextMethod("byPath");
 }, static=TRUE)
 
 
-setMethodS3("findByName", "PairedPSCBSFileSet", function(static, ..., chipType=NULL, paths="pscbsData/") {
+setMethodS3("findByName", "PairedPSCBSFileSet", function(static, ..., chipType, paths="pscbsData/") {
   # Argument 'chipType':
-  if (!is.null(chipType)) {
-    chipType <- Arguments$getCharacter(chipType);
-  }
+  chipType <- Arguments$getCharacter(chipType);
 
   # Argument 'paths':
-  if (is.null(paths)) {
-    paths <- eval(formals(findByName.PairedPSCBSFileSet)[["paths"]]);
-  }
+  if (!is.null(paths)) paths <- Arguments$getCharacters(paths);
 
-  # Drop argument 'subdirs'
-  args <- list(...);
-  excl <- which("subdirs" == names(args));
-  if (length(excl) > 0) {
-    args <- args[-excl];
-  }
-  args <- c(list(static=static), args, subdirs=chipType, paths=paths);
-
-  # Call the "next" method
-  args <- c(list("findByName"), args);
-  do.call("NextMethod", args);
+  NextMethod("findByName", subdirs=chipType, paths=paths);
 }, static=TRUE, protected=TRUE)
 
 
-setMethodS3("byName", "PairedPSCBSFileSet", function(static, name, ..., paths=NULL) {
- path <- findByName(static, name=name, ..., paths=paths);
+setMethodS3("byName", "PairedPSCBSFileSet", function(static, name, ...) {
+ path <- findByName(static, name=name, ...);
  if (is.null(path)) {
-   throw("Failed to locate ", class(static)[1]);
+   throw("Failed to locate ", class(static)[1L]);
  }
 
- byPath(static, path=path, ...);
+  # Call byPath without argument 'chipType'
+  args <- list(static, path=path, ...);
+  excl <- which("chipType" == names(args));
+  if (length(excl) > 0) args <- args[-excl];
+  do.call(byPath, args=args);
 })
 
 
@@ -80,6 +99,11 @@ setMethodS3("getChipType", "PairedPSCBSFileSet", function(this, ...) {
 
 #############################################################################
 # HISTORY:
+# 2013-08-12
+# o BUG FIX: byPath(), byName() and findByPath() for PairedPSCBSFileSet
+#   was also affected by the bug described in the R-devel thread
+#   'Do *not* pass '...' to NextMethod() - it'll do it for you; missing
+#   documentation, a bug or just me?' on Oct 16, 2012.
 # 2012-09-19
 # o Added getDefaultFullName().
 # o AD HOC: Added getChipType() and getPlatform(). Just so that this object
