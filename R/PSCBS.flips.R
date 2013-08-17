490042f0f3b8fa54d19b2aa7250083b9bf6ba3c0
Henrik Bengtsson <hb@aroma-project.org>
2013-08-17 17:17:43 +0000

    Version: 1.4.2 [2013-08-15] o More internal updates.
    Version: 1.4.1 [2013-08-12]
    o BUG FIX: byPath(), byName() and findByPath() for PairedPSCBSFileSet
      was also affected by the bug described in the R-devel thread
      'Do *not* pass '...' to NextMethod() - it'll do it for you; missing
      documentation, a bug or just me?' on Oct 16, 2012.
    o BUG FIX: getPath() for PairedPscbsModel would throw an error on
      getInputDataSet() not defined. 
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@3145 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/PSCBS.flips.R
@@ -0,0 +1,160 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# For (C1,C2) data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+setMethodS3("estimateChangePointFlips", "matrix", function(C, ...) {
+  estimateChangePointFlips(as.data.frame(C), ...);
+})
+
+
+# TODO: Return/incorporate information on flanking segment lengths.
+#       This is part of 'data', but not used and not returned right
+#       now.  Decision is to ignore that now.  Maybe we'll later
+#       use bootstrapping or similar to estimate confidence scores
+#       which in turn may affect the call. /HB+PN 2013-08-15 in Banff.
+setMethodS3("estimateChangePointFlips", "data.frame", function(data, ...) {
+  n <- nrow(data);
+  nbrOfCPs <- n - 1L;
+  alpha <- matrix(NA_real_, nrow=nbrOfCPs, ncol=2L);
+  colnames(alpha) <- c("alpha0", "alpha1");
+
+  # (C1,C2)
+  C <- as.matrix(data[,1:2,drop=FALSE]);
+
+  # For each change point
+  for (ii in seq_len(nbrOfCPs)) {
+    rows <- ii + 0:1;
+
+    # alpha0: angle without flip
+    C0 <- C[rows,];
+    D0 <- C0[2,] - C0[1,];
+    alpha0 <- atan(D0[1]/D0[2]);
+
+    # alpha1: angle with flip
+    C1 <- C0; C1[2,] <- C1[2,2:1];
+    D1 <- C1[2,] - C1[1,];
+    alpha1 <- atan(D1[1]/D1[2]);
+
+    alpha[ii,1L] <- alpha0;
+    alpha[ii,2L] <- alpha1;
+  } # for (ii ...)
+
+  betaA <- abs(alpha - pi/4);
+  colnames(betaA) <- c("betaA0", "betaA1");
+  betaB <- abs(alpha + pi/4);
+  colnames(betaB) <- c("betaB0", "betaB1");
+  gammaA <- pmin(betaA[,1], betaB[,1]);
+  gammaB <- pmin(betaA[,2], betaB[,2]);
+  call <- (gammaB > gammaA);
+  cbind(alpha, betaA=betaA, betaB=betaB, gammaA=gammaA, gammaB=gammaB, call=call);
+})
+
+
+setMethodS3("callChangePointFlips", "matrix", function(C, ...) {
+  callChangePointFlips(as.data.frame(C, ...));
+}, protected=TRUE)
+
+setMethodS3("callChangePointFlips", "data.frame", function(C, ...) {
+  theta <- estimateChangePointFlips(C, ...);
+  flip <- as.logical(theta[,"call"]);
+  which(flip) + 1L;
+}, protected=TRUE)
+
+setMethodS3("callChangePointFlips", "PSCBS", function(fit, ...) {
+  C <- extractC1C2(fit);
+  flips <- callChangePointFlips(C);
+  flips;
+})
+
+
+setMethodS3("adjustAB", "PSCBS", function(fit, ...) {
+  segs <- getSegments(fit);
+  segs$dhMean[segs$abCall] <- 0;
+  fit$output <- segs;
+  params <- fit$params;
+  params$abAdjusted <- TRUE;
+  fit$params <- params;
+  fit;
+})
+
+
+setMethodS3("flipChangePoints", "PSCBS", function(fit, flips=callChangePointFlips(fit), ...) {
+  # Argument 'flips':
+  flips <- Arguments$getIndices(flips, max=nbrOfSegments(fit, splitters=TRUE));
+
+  # Update segment data
+  if (length(flips) > 0L) {
+    segs <- getSegments(fit);
+    fields <- colnames(segs);
+
+    # (a) Update (C1,C2) fields - swap (C1,C2)
+    c1Names <- grep("^c1", fields, value=TRUE);
+    c2Names <- gsub("^c1", "c2", c1Names);
+    stopifnot(length(c2Names) == length(c1Names));
+
+    for (kk in seq_along(c1Names)) {
+      cols <- c(c1Names[kk], c2Names[kk]);
+      C <- segs[,cols];
+      C[flips,] <- C[flips,2:1];
+      segs[,cols] <- C;
+    }
+
+##    # (b) Update DH fields - swap signs
+##    dhNames <- grep("^dh", fields, value=TRUE);
+##    dhNames <- setdiff(dhNames, c("dhId", "dhStart", "dhEnd", "dhNbrOfLoci"));
+##    for (name in dhNames) {
+##      segs[[name]] <- -segs[[name]];
+##    }
+
+    segs$c1c2Swap <- FALSE;
+    segs$c1c2Swap[flips] <- TRUE;
+
+    fit$output <- segs;
+  }
+
+  # Update parameter estimates
+  params <- fit$params;
+  params$flipChangePoints <- list(
+    flips = flips
+  );
+  fit$params <- params;
+
+  fit;
+})
+
+
+
+setMethodS3("deshearC1C2", "PSCBS", function(fit, S=0, ...) {
+  # Argument 'S':
+  S <- Arguments$getNumeric(S);
+
+  # Deshear (C1,C2) which is dual to adjusting DH.
+  segs <- getSegments(fit, splitters=TRUE);
+  fields <- colnames(segs);
+
+  # Update DH fields
+  dhNames <- grep("^dh", fields, value=TRUE);
+  dhNames <- setdiff(dhNames, c("dhId", "dhStart", "dhEnd", "dhNbrOfLoci"));
+  for (name in dhNames) {
+    rho <- segs[[name]];;
+    rho <- (1+S)/(1-S)*rho;
+    segs[[name]] <- -rho;
+  }
+
+  fit$output <- segs;
+
+  # Update parameters
+  params <- fit$params;
+  params$deshearC1C2 <- list(S=S);
+  fit$params <- params;
+
+  fit;
+})
+
+
+##############################################################################
+# HISTORY
+# 2013-08-15
+# o Added to aroma.cn.
+# 2013-08-09
+# o Created.
+##############################################################################
