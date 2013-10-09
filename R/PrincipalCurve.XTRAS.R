5bc6e2c8d0cc18b5bcb8f6d04379b87f56aa2b5e
Henrik Bengtsson <hb@aroma-project.org>
2013-10-09 00:48:41 +0000

    o Added arguments 'col', 'lwd' and 'lty' to plotBacktransforms() and   argument 'xlim' now defaults to the maximum range of the data.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.cn@3243 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/PrincipalCurve.XTRAS.R
+++ R/PrincipalCurve.XTRAS.R
@@ -1,63 +1,70 @@
-setMethodS3("getBacktransforms", "PrincipalCurve", function(fit, dimensions=NULL, targetDimension=1, range=NULL, length.out=100, ...) {
+setMethodS3("getBacktransforms", "PrincipalCurve", function(fit, dimensions=NULL, targetDimension=1L, range=NULL, length.out=100L, ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Argument 'dimensions':
   s <- fit$s;
   ndim <- ncol(s);
-  if (is.null(dimensions)) { 
+  if (is.null(dimensions)) {
     dimensions <- seq_len(ndim);
   }
   dimensions <- Arguments$getIndices(dimensions, max=ndim);
 
   # Argument 'range':
   if (is.null(range)) {
-    range <- range(s, na.rm=TRUE);  
+    range <- range(s, na.rm=TRUE);
   }
-  range <- Arguments$getDoubles(range, length=c(2,2), disallow=c("NA", "NaN"));
+  range <- Arguments$getDoubles(range, length=c(2L,2L), disallow=c("NA", "NaN"));
 
 
-  y <- seq(from=range[1], to=range[2], length.out=length.out);
+  y <- seq(from=range[1L], to=range[2L], length.out=length.out);
 
   naValue <- as.double(NA);
-  dim <- c(length(y), 2, length(dimensions));
+  dim <- c(length(y), 2L, length(dimensions));
   XY <- array(naValue, dim=dim);
 
   for (kk in seq_along(dimensions)) {
     dim <- dimensions[kk];
     yN <- backtransformPrincipalCurve(y, fit=fit, dimensions=dim,
-                                     targetDimension=targetDimension);
-    yN <- yN[,1,drop=TRUE];
+                                      targetDimension=targetDimension);
+    yN <- yN[,1L,drop=TRUE];
     xy <- cbind(y, yN);
 
     XY[,,kk] <- xy;
   } # for (kk ...)
 
   XY;
-})
+}) # getBacktransforms()
 
-setMethodS3("plotBacktransforms", "PrincipalCurve", function(fit, ..., xlim=c(-3,3), ylim=xlim, xlab="y", ylab="y*") {
+
+setMethodS3("plotBacktransforms", "PrincipalCurve", function(fit, ..., col="red", lwd=2, lty=1L, xlim=NULL, ylim=xlim, xlab="y", ylab="y*") {
   XY <- getBacktransforms(fit, ...);
 
   if (is.null(xlim)) {
-    range(XY, na.rm=FALSE);
+    xlim <- range(XY, na.rm=FALSE);
   }
   if (is.null(ylim)) {
     ylim <- xlim;
   }
 
-  ndim <- dim(XY)[3];
+  ndim <- dim(XY)[3L];
   subplots(ndim);
   for (kk in seq_len(ndim)) {
     xy <- XY[,,kk];
     plot(NA, xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab);
-    abline(a=0, b=1, lty=3);
-    lines(xy, col="red", lwd=2);
+    abline(a=0, b=1, lty=3L);
+    lines(xy, col=col, lwd=lwd, lty=lty);
   }
 
   invisible(XY);
-})
+}) # plotBacktransforms()
 
 
 ############################################################################
 # HISTORY:
+# 2010-10-08
+# o Added arguments 'col', 'lwd' and 'lty' to plotBacktransforms() and
+#   argument 'xlim' now defaults to the maximum range of the data.
 # 2010-01-14
 # o Added getBacktransforms() and plotBacktransforms().
 # o Created.
