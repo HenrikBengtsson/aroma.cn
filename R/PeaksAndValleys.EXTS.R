2478f216999abff1e69fced1e55e2a67cde2aa7b
hb <hb@aroma-project.org>
2015-10-27 11:58:30 -0700

    CLEANUP: Moved deprecated/defunct methods to separate file

--- R/PeaksAndValleys.EXTS.R
+++ R/PeaksAndValleys.EXTS.R
@@ -137,19 +137,6 @@ setMethodS3("callPeaks", "PeaksAndValleys", function(fit, expected=c(-1/2,-1/4,0
 
 
 
-setMethodS3("callPeaks", "data.frame", function(fit, ...) {
-  ## Defunct 2015-09-18
-  .Defunct(msg="callPeaks() for data.frame:s is defunct; use ditto for PeaksAndValleys objects. It will eventually be removed from the package.");
-
-  # Argument 'fit';
-  stopifnot(all(is.element(c("type", "x", "density"), colnames(fit))));
-  class(fit) <- c("PeaksAndValleys", class(fit));
-  callPeaks(fit, ...);
-}, private=TRUE, deprecated=TRUE)
-
-
-
-
 ##############################################################################
 # HISTORY
 # 2013-08-04 [HB]
